2e3575097c851cd50bef8e0b5f7ed98e4e043f3c
Ray <rayarindam2111@gmail.com>
2021-05-27 22:41:50 +0530

    node update

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'), app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname));
+
+http.listen(port, function () {
+	console.log('Listening on port ' + port);
+});
