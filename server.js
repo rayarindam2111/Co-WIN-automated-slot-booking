11660558d185586a1de65d98df3b94faae14f95f
Ray <rayarindam2111@gmail.com>
2021-05-29 22:09:02 +0530

    ssl update

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express'), app = express();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+	if (req.get('X-Forwarded-Proto') == 'https' || req.hostname == 'localhost')
+		next();
+	else if (req.get('X-Forwarded-Proto') != 'https' && req.get('X-Forwarded-Port') != '443')
+		res.redirect('https://' + req.hostname + req.url);
+});
+
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
